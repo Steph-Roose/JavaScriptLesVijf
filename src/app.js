054a96a5b888a24c83232437163989dddfec7f9e
Steph-Roose <steph241291@gmail.com>
2022-02-09 11:49:15 +0100

    huiswerk af

--- src/app.js
+++ src/app.js
@@ -2,19 +2,20 @@ import axios from "axios";
 
 console.log('Hallo daar!');
 
-//PSEUDO CODE
-//  1. Importeer axios.
-//  2. Gebruik axios om api van restcountries binnen te krijgen.
-//      ! Gebruik een try/catch om eventuele errors op te vangen.
-//      ! Controleer of de api werkt met een console.log.
-//  3. Sorteer alle landen van minste naar meeste inwoners (sort).
-//  4. Creëer een functie waarmee ik een lijst van alle landen weer kan geven op de pagina.
-//      a. Maak een element aan in index.html dat ik als anker kan gebruiken.
-//      b. Maak een HTML-element aan voor de vlag van het land (img).
-//      c. Maak een HTML-element aan voor de naam van het land (h3).
-//      d. Maak een HTML-element aan voor het aantal inwoners van het land (p).
-//  5. Creëer een functie waarmee ik landen van iedere regio anders kleur.
-//  6. Zorg ervoor dat de pagina er aantrekkelijk uitziet.
+/*PSEUDO CODE
+   1. Importeer axios.
+   2. Maak een try/catch functie aan om met axios en het endpoint de lijst met landen binnen te halen.
+   3. Controleer of ik de lijst goed binnenkrijgt met een console.log.
+   4. Gebruik de map-methode om een array van alle vlaggen te genereren.
+   5. Gebruik de map-methode om een array van alle namen te genereren.
+   6. Gebruik de map-methode om een array van alle inwonersaantallen te genereren.
+   7. Gebruik de map-methode om een array van alle regios te genereren en aan iedere regio een andere class toe te wijzen.
+   8. Schrijf een functie waarmee ik 4-6 kan combineren en weergeven op de pagina.
+       a. wrap 4 in een <img> element.
+       b. wrap 5 in een <h3> element. Gebruik de methode uit 7 om class toe te wijzen.
+       c. wrap 6 in een <p> element.
+   9. Pas styling aan in styles.css
+*/
 
 // Haal lijst met landen binnen.
 async function getCountries() {
@@ -27,7 +28,7 @@ async function getCountries() {
             return a.population - b.population;
         })
 
-        generateCountryList(result.data);
+        generateCountriesList(result.data);
 
     } catch (e) {
         console.error(e);
@@ -36,24 +37,62 @@ async function getCountries() {
 
 getCountries();
 
-// Creëer functie voor weergave op pagina.
-const countryList = document.getElementById('countries-list');
-const countriesList = document.createElement('li');
-
-function generateCountryList(arr) {
-    return arr.map((country) => {
-        countriesList.innerHTML = `
-            <img src="${country.flag}" class="flag" alt="flag"/>
-            <h3 class="${country.region}">${country.name}</h3>
-            <p class="population">Has a population of ${country.population} people.</p>
-        `
-        countryList.appendChild(countriesList);
-    });
+// Maak een lijst met vlaggen.
+function getFlags(flag) {
+    return `${flag}`;
 }
 
-// Kleur landen per regio.
-function colorCountries(arr) {
-    const Africa = arr.filter((country) => {
+// Maak een lijst met land-namen.
+function getNames(name) {
+    return `${name}`;
+}
 
+// Maak en lijst met inwonersaantallen.
+function getPopulation(population) {
+    return `Has a population of ${population} people`;
+}
+
+// Maak een lijst met regio's.
+function getRegion (region) {
+    if (region === "Africa" ) {
+        return "africa";
+    } else if (region === "Americas" ) {
+        return "americas";
+    } else if (region === "Asia" ) {
+        return "asia";
+    } else if (region === "Europe" ) {
+        return "europe";
+    } else {
+        return "oceania";
+    }
+}
+
+// Combineer de gemaakte lijsten.
+function generateCountriesList (arr) {
+    arr.map((country) => {
+        const countriesList = document.getElementById("countries-list");
+        const countryListing = document.createElement("li");
+
+        const countryFlag = document.createElement("img");
+        countryFlag.setAttribute("class", "flag");
+        countryFlag.src = getFlags(country.flag);
+        countryListing.appendChild(countryFlag);
+
+        const countryName = document.createElement("h4");
+        countryName.setAttribute("class", "name");
+        countryName.textContent = getNames(country.name);
+        countryListing.appendChild(countryName);
+
+        const countryPopulation = document.createElement("p");
+        countryPopulation.setAttribute("class", "population");
+        countryPopulation.textContent = getPopulation(country.population);
+        countryListing.appendChild(countryPopulation);
+
+        let att = document.createAttribute("class");
+        att.value = getRegion(country.region);
+        countryName.setAttributeNode(att);
+
+        return countriesList.appendChild(countryListing);
     })
 }
+
